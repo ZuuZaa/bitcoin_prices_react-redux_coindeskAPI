8975d400dfa830481d2d3842bce6b77e0627de4a
ZuuZaa <zumbaghirova@gmail.com>
2022-10-12 15:24:03 +0400

    favicon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { store } from 'store';
 
 function App() {
@@ -8,20 +6,7 @@ function App() {
   console.log(store)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+app
     </div>
   );
 }
